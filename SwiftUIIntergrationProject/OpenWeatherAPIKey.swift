b8c2e47544c3548ecf82fc0f1c8d1d6ec0220333
kptech80 <kptech80@gmail.com>
2024-06-26 08:57:07 +0800

    update openWeatherAPIKey API Key

--- SwiftUIIntergrationProject/OpenWeatherAPIKey.swift
+++ SwiftUIIntergrationProject/OpenWeatherAPIKey.swift
@@ -8,4 +8,4 @@
 import Foundation
 
 //TODO: Enter your Open weather API Key from: https://home.openweathermap.org/api_keys
-let openWeatherAPIKey: String = ""
+let openWeatherAPIKey: String = "9fb9e3a1cfc27ff6e0c212107b8af274"
